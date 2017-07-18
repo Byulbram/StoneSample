b979b2f3aa702cafe417e1595816b2174d386e5a
Byulbram" KwangSam Kim <byulbram@ck.ac.kr>
2017-07-18 17:21:26 +0900

    Adding functions

--- Assets/StoneManager.cs
+++ Assets/StoneManager.cs
@@ -9,12 +9,14 @@ using System.Text;
 using SnailLauncherSDK_CS; //if This line got error, check SnailLauncherSDK_Proxy_CS.dll's target. one for x86, other for x86_x64
 
 public class StoneManager : MonoBehaviour {
+    public static StoneManager SM;
     public Text Console;    
     private StringBuilder ConsoleText = new StringBuilder();
 
     // Use this for initialization
     void Start ()
     {
+        SM = this;
         ConsoleText.Append(Console.text);
         UpdateConsole("Program Started");
 	}
@@ -54,14 +56,124 @@ public class StoneManager : MonoBehaviour {
             bool bRet = SnailLauncher_CS.GetLauncher();
             if (bRet == true)
             {
-                UpdateConsole("Get Launcher OK");
+                UpdateConsole("Get Launcher OK!!");
             }
             else
             {
-                UpdateConsole("Get Launcher Fail");
+                UpdateConsole("Get Launcher Failed");
             }
         }
     }
+
+    public void LaunchClientInterface()
+    {
+        UpdateConsole("Start LaunchClientInterface");
+        E_StartLauncher_Result_CS startResult = SnailLauncher_CS.StartLauncherW("The Silver Bullet: Prometheus", 30171, null);
+        if (startResult == 0)
+        {
+            UpdateConsole("StartLauncher OK!!");
+        }
+        else
+        {
+            UpdateConsole("StartLauncher Failed : " + startResult);
+        }
+    }
+
+
+    public void GetServiceTicketSync()
+    {
+        UpdateConsole("Start GetServiceTicketSync");
+        int iErrCode = 0;
+        SnailLauncherSDK_CS.StServiceTicketInfo_CS stTemp = SnailLauncher_CS.GetServiceTicket_Sync(ref iErrCode);
+        UpdateConsole("Result : " + stTemp);
+    }
+
+    public void GetServiceTicketASync()
+    {
+        UpdateConsole("Start GetServiceTicketASync");
+        E_SdkCodeError_CS retVal = SnailLauncher_CS.GetServiceTicket_ASync(onServiceTicketCallback);
+        UpdateConsole("Result : " + retVal);
+    }
+
+    private static void onServiceTicketCallback(StServiceTicketInfo_CS ticket, E_SdkCodeError_CS error)
+    {
+        string strServiceTicket = System.Text.Encoding.Default.GetString(ticket.szServiceTicket);
+        SM.UpdateConsole(strServiceTicket.ToString());
+    }
+
+    public void RegisterStatusCheck()
+    {
+        UpdateConsole("Start RegisterStatusCheck");
+        E_SdkCodeError_CS retVal = SnailLauncher_CS.RegisterStatusCheckCallback(OnStatusCheckCallback);
+        UpdateConsole("Result : " + retVal);
+    }
+
+    private static void OnStatusCheckCallback(EStatusCheck_CS status)
+    {
+        switch (status)
+        {
+            case EStatusCheck_CS.SUCCESS: SM.UpdateConsole("OnStatusCheckCallback, status is ESuccess ."); break;
+
+            case EStatusCheck_CS.ERROR: SM.UpdateConsole("OnStatusCheckCallback, status is EError ."); break;
+
+            case EStatusCheck_CS.EXIT: SM.UpdateConsole("OnStatusCheckCallback, status is EExit ."); break;
+
+            default:
+                SM.UpdateConsole("OnStatusCheckCallback , status is UnKnown !"); break;
+        }
+    }
+
+    public void RegisterAntiAddiction()
+    {
+        UpdateConsole("Start RegisterAntiAddiction");
+        E_SdkCodeError_CS retVal = SnailLauncher_CS.RegisterAntiAddictionCallback(OnAntiAddictionCallback); SnailLauncher_CS.ReportPlayTheGameTimeReachCycle(10);
+        UpdateConsole("Result : " + retVal);
+    }
+
+
+    private static void OnAntiAddictionCallback(S_AntiAddiction_CS antiAddiction)
+    {
+        switch (antiAddiction.type)
+        {
+            case E_AntiAddiction_CS.OK:
+                //SnailLauncher_CS.Shutdown();
+                //TerminateApp();
+                SM.UpdateConsole("OnAntiAddictionCallback, Non anti-addiction account(EAntiAddictionOK).");
+                break;
+            case E_AntiAddiction_CS.A_FORCE_GAME_END:
+                SM.UpdateConsole("OnAntiAddictionCallback,  anti-addiction  game  play  time  reach(EAntiAddictionForceGameEnd).");
+                break;
+
+            case E_AntiAddiction_CS.A_180_Minutes:
+                SM.UpdateConsole("OnAntiAddictionCallback,  anti-addiction  game  play  time	180  minutes(E AntiAddiction180Minutes).");
+                break;
+
+            case E_AntiAddiction_CS.A_120_Minutes:
+                SM.UpdateConsole("OnAntiAddictionCallback,  anti-addiction  game  play  time	120  minutes(E AntiAddiction120Minutes).");
+                break;
+
+            case E_AntiAddiction_CS.A_60_Minutes:
+                SM.UpdateConsole("OnAntiAddictionCallback,  anti-addiction  game  play  time	60  minutes(EA ntiAddiction60Minutes).");
+                break;
+
+            case E_AntiAddiction_CS.A_PLAY_GAME:
+                SM.UpdateConsole("OnAntiAddictionCallback,anti-addiction is EAntiAddictionPlayGame ."); break;
+
+            default:
+                SM.UpdateConsole("OnAntiAddictionCallback, antiAddiction.type is UnKnown !"); break;
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        SnailLauncher_CS.FreeLaunchInstance();
+        bool bRet = SnailLauncher_CS.Shutdown();
+        Debug.Log("Shutdown result = " + bRet);
+    }
+
+
+
+
 
 
 }
