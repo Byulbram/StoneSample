4fefa570c1f998d0d9fe3cecfb9b1748c81b0edb
Byulbram" KwangSam Kim <byulbram@ck.ac.kr>
2017-07-18 16:16:11 +0900

    added comment

--- Assets/StoneManager.cs
+++ Assets/StoneManager.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿//Stone SDK Sample 
+//by Byulbram 2017.07.18
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Text;
-using SnailLauncherSDK_CS;
+using SnailLauncherSDK_CS; //if This line got error, check SnailLauncherSDK_Proxy_CS.dll's target. one for x86, other for x86_x64
 
 public class StoneManager : MonoBehaviour {
     public Text Console;    
@@ -32,17 +35,19 @@ public class StoneManager : MonoBehaviour {
     public void Initialize()
     {
         UpdateConsole("Start Stone SDK initialize");
+
+        //Get Unity Application Path. 
+        //SnailLauncherSDK.dll, SnailLauncherSDK_Proxy_C.dll should be at Project folder in Editor and exe folder at build.
         string ApplicationPath = Application.dataPath;
         ApplicationPath += "/../";
         
         UpdateConsole(ApplicationPath);
         E_InitResult_CS eRet = SnailLauncher_CS.LoadLibraries(ApplicationPath);
 
-
         
         if (eRet != E_InitResult_CS.OK)
         {
-            UpdateConsole("Load STONE SDK Failed: " + eRet);
+            UpdateConsole("Load STONE SDK Failed: " + eRet); // returns error code
         }
         else
         {
@@ -53,7 +58,7 @@ public class StoneManager : MonoBehaviour {
             }
             else
             {
-                UpdateConsole("Get Launver Fail");
+                UpdateConsole("Get Launcher Fail");
             }
         }
     }
